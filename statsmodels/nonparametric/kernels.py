35d260b9c1fe7b3cd64604855aa67721edbbef23
Pierre Barbier de Reuille <pierre.barbierdereuille@gmail.com>
2014-08-28 02:55:21 +0200

    Added CDF in nD
    Note: need to invert axis for data

--- statsmodels/nonparametric/kernels.py
+++ statsmodels/nonparametric/kernels.py
@@ -12,7 +12,8 @@ import numpy as np
 from scipy.special import erf
 from scipy import fftpack, integrate
 from .kde_utils import make_ufunc, numpy_method_idx
-from . import _kernels as kernels_imp
+from . import _kernels
+from copy import copy as shallowcopy
 
 S2PI = np.sqrt(2 * np.pi)
 
@@ -109,17 +110,15 @@ class Kernel1D(object):
         try:
             comp_pdf = self.__comp_pdf
         except AttributeError:
-            def pdf(x):
-                return self.pdf(np.atleast_1d(x))
             lower = self.lower
             upper = self.upper
             @make_ufunc()
             def comp_pdf(x):
-                if x < lower:
+                if x <= lower:
                     return 0
-                if x > upper:
+                if x >= upper:
                     x = upper
-                return integrate.quad(pdf, lower, x)[0]
+                return integrate.quad(self.pdf, lower, x)[0]
             self.__comp_cdf = comp_pdf
         if out is None:
             out = np.empty(z.shape, dtype=float)
@@ -140,7 +139,7 @@ class Kernel1D(object):
             lower = self.lower
             upper = self.upper
             def pm1(x):
-                return x * self.pdf(np.atleast_1d(x))
+                return x * self.pdf(x)
             @make_ufunc()
             def comp_pm1(x):
                 if x <= lower:
@@ -168,7 +167,7 @@ class Kernel1D(object):
             lower = self.lower
             upper = self.upper
             def pm2(x):
-                return x * x * self.pdf(np.atleast_1d(x))
+                return x * x * self.pdf(x)
             @make_ufunc()
             def comp_pm2(x):
                 if x <= lower:
@@ -245,7 +244,7 @@ class normal_kernel1d(Kernel1D):
         :param ndarray xs: Array of any shape
         :returns: an array of shape identical to ``xs``
         """
-        return kernels_imp.norm1d_pdf(z, out)
+        return _kernels.norm1d_pdf(z, out)
 
     def _pdf(self, z, out=None):
         """
@@ -309,7 +308,7 @@ class normal_kernel1d(Kernel1D):
             \text{cdf}(z) \triangleq \int_{-\infty}^z \phi(z)
                 dz = \frac{1}{2}\text{erf}\left(\frac{z}{\sqrt{2}}\right) + \frac{1}{2}
         """
-        return kernels_imp.norm1d_cdf(z, out)
+        return _kernels.norm1d_cdf(z, out)
 
     def _cdf(self, z, out=None):
         """
@@ -333,7 +332,7 @@ class normal_kernel1d(Kernel1D):
             \text{pm1}(z) \triangleq \int_{-\infty}^z z\phi(z) dz
                 = -\frac{1}{\sqrt{2\pi}}e^{-\frac{z^2}{2}}
         """
-        return kernels_imp.norm1d_pm1(z, out)
+        return _kernels.norm1d_pm1(z, out)
 
     def _pm1(self, z, out=None):
         """
@@ -358,7 +357,7 @@ class normal_kernel1d(Kernel1D):
                 = \frac{1}{2}\text{erf}\left(\frac{z}{2}\right) - \frac{z}{\sqrt{2\pi}}
                 e^{-\frac{z^2}{2}} + \frac{1}{2}
         """
-        return kernels_imp.norm1d_pm2(z, out)
+        return _kernels.norm1d_pm2(z, out)
 
     def _pm2(self, z, out=None):
         """
@@ -379,8 +378,88 @@ class normal_kernel1d(Kernel1D):
         out += 0.5
         return out
 
+class KernelnD(object):
+    cut = 3.
+    lower = -np.inf
+    upper = np.inf
 
-class normal_kernel(object):
+    def __init__(self, ndim=2):
+        self._ndim = ndim
+
+    @property
+    def ndim(self):
+        return self._ndim
+
+    def for_ndim(self, ndim):
+        """
+        Create a version of the same kernel, but for dimension ``ndim``
+        """
+        if ndim == self.ndim:
+            return self
+        new_ker = shallowcopy(self)
+        new_ker._ndim = ndim
+        return new_ker
+
+    def pdf(self, y, out=None):
+        r"""
+        Returns the density of the kernel on the points `z`. This is the funtion :math:`K(z)` itself.
+
+        Parameters
+        ----------
+        z: ndarray
+            Array of points to evaluate the function on. This should be at 
+            least a 2D array, with the first dimension corresponding to the 
+            dimension of the problem.
+        out: ndarray
+            If provided, it will be of the same shape as `z` and the result 
+            should be stored in it. Ideally, it should be used for as many 
+            intermediate computation as possible.
+        """
+        raise NotImplementedError()
+
+    def __call__(self, z, out=None):
+        """
+        Alias for :py:meth:`KernelnD.pdf`
+        """
+        return self.pdf(z, out=out)
+
+    def cdf(self, z, out=None):
+        z = np.asfarray(z)
+        try:
+            comp_pdf = self.__comp_pdf
+        except AttributeError:
+            def pdf(*xs):
+                return self.pdf(xs)
+            lower = self.lower
+            upper = self.upper
+            ndim = self.ndim
+            if ndim == 2:
+                @make_ufunc()
+                def comp_pdf2d(x, y):
+                    if x <= lower or y <= lower:
+                        return 0
+                    if x > upper:
+                        x = upper
+                    if y > upper:
+                        y = upper
+                    return integrate.dblquad(pdf, lower, y,
+                                             lambda y:lower, lambda y: x)[0]
+                comp_pdf = comp_pdf2d
+                self.__comp_pdf = comp_pdf2d
+            else:
+                @make_ufunc(ndim)
+                def comp_pdfnd(*xs):
+                    if np.any(xs <= lower):
+                        return 0
+                    xs = np.minimum(xs, upper)
+                    return integrate.nquad(pdf, [(lower, x) for x in xs])[0]
+                comp_pdf = comp_pdfnd
+                self.__comp_pdf = comp_pdfnd
+        if out is None:
+            out = np.empty(z.shape[1:], dtype=float)
+        return comp_pdf(*z, out=out)
+
+class normal_kernel(KernelnD):
     """
     Returns a function-object for the PDF of a Normal kernel of variance
     identity and average 0 in dimension ``dim``.
@@ -396,9 +475,10 @@ class normal_kernel(object):
         return normal_kernel(ndim)
 
     def __init__(self, dim=2):
+        super(normal_kernel, self).__init__(dim)
         self.factor = 1 / np.sqrt(2 * np.pi) ** dim
 
-    def pdf(self, xs):
+    def pdf(self, xs, out=None):
         """
         Return the probability density of the function.
 
@@ -406,247 +486,33 @@ class normal_kernel(object):
             and N the number of points.
         :returns: an array of shape (N,) with the density on each point of ``xs``
         """
-        xs = np.atleast_2d(xs)
-        return self.factor * np.exp(-0.5 * np.sum(xs * xs, axis=0))
-
-    __call__ = pdf
-
-
-class tricube(Kernel1D):
-    r"""
-    Return the kernel corresponding to a tri-cube distribution, whose expression is.
-    The tri-cube function is given by:
-
-    .. math::
-
-        f_r(x) = \left\{\begin{array}{ll}
-                        \left(1-|x|^3\right)^3 & \text{, if } x \in [-1;1]\\
-                                0 & \text{, otherwise}
-                        \end{array}\right.
-
-    As :math:`f_r` is not a probability and is not of variance 1, we use a normalized function:
-
-    .. math::
-
-        f(x) = a b f_r(ax)
-
-        a = \sqrt{\frac{35}{243}}
-
-        b = \frac{70}{81}
-
-    """
-
-    def pdf(self, z, out=None):
-        return kernels_imp.tricube_pdf(z, out)
-
-    __call__ = pdf
-
-    upper = 1. / kernels_imp.tricube_width
-    lower = -upper
-    cut = upper
-
-    def cdf(self, z, out=None):
-        r"""
-        CDF of the distribution:
-
-        .. math::
-
-            \text{cdf}(x) = \left\{\begin{array}{ll}
-                \frac{1}{162} {\left(60 (ax)^{7} - 7 {\left(2 (ax)^{10} + 15 (ax)^{4}\right)}
-                \mathrm{sgn}\left(ax\right) + 140 ax + 81\right)} & \text{, if}x\in[-1/a;1/a]\\
-                0 & \text{, if} x < -1/a \\
-                1 & \text{, if} x > 1/a
-                \end{array}\right.
-        """
-        return kernels_imp.tricube_cdf(z, out)
-
-    def pm1(self, z, out=None):
-        r"""
-        Partial moment of order 1:
-
-        .. math::
-
-            \text{pm1}(x) = \left\{\begin{array}{ll}
-                \frac{7}{3564a} {\left(165 (ax)^{8} - 8 {\left(5 (ax)^{11} + 33 (ax)^{5}\right)}
-                \mathrm{sgn}\left(ax\right) + 220 (ax)^{2} - 81\right)}
-                & \text{, if} x\in [-1/a;1/a]\\
-                0 & \text{, otherwise}
-                \end{array}\right.
-        """
-        return kernels_imp.tricube_pm1(z, out)
-
-    def pm2(self, z, out=None):
-        r"""
-        Partial moment of order 2:
-
-        .. math::
-
-            \text{pm2}(x) = \left\{\begin{array}{ll}
-            \frac{35}{486a^2} {\left(4 (ax)^{9} + 4 (ax)^{3} - {\left((ax)^{12} + 6 (ax)^{6}\right)}
-            \mathrm{sgn}\left(ax\right) + 1\right)} & \text{, if} x\in[-1/a;1/a] \\
-            0 & \text{, if } x < -1/a \\
-            1 & \text{, if } x > 1/a
-            \end{array}\right.
-        """
-        return kernels_imp.tricube_pm2(z, out)
-
-
-class Epanechnikov(Kernel1D):
-    r"""
-    1D Epanechnikov density kernel with extra integrals for 1D bounded kernel estimation.
-    """
-    def pdf(self, xs, out=None):
-        r"""
-        The PDF of the kernel is usually given by:
-
-        .. math::
-
-            f_r(x) = \left\{\begin{array}{ll}
-                            \frac{3}{4} \left(1-x^2\right) & \text{, if} x \in [-1:1]\\
-                                    0 & \text{, otherwise}
-                            \end{array}\right.
-
-        As :math:`f_r` is not of variance 1 (and therefore would need adjustments for
-        the bandwidth selection), we use a normalized function:
-
-        .. math::
-
-            f(x) = \frac{1}{\sqrt{5}}f\left(\frac{x}{\sqrt{5}}\right)
-        """
-        return kernels_imp.epanechnikov_pdf(xs, out)
-    __call__ = pdf
-
-    upper = 1. / kernels_imp.epanechnikov_width
-    lower = -upper
-    cut = upper
+        xs = np.asarray(xs)
+        if out is None:
+            out = np.empty(xs.shape[1:], dtype=float)
+        np.sum(xs*xs, axis=0, out=out)
+        out *= -0.5
+        np.exp(out, out=out)
+        out *= self.factor
+        return out
 
     def cdf(self, xs, out=None):
-        r"""
-        CDF of the distribution. The CDF is defined on the interval :math:`[-\sqrt{5}:\sqrt{5}]` as:
-
-        .. math::
-
-            \text{cdf}(x) = \left\{\begin{array}{ll}
-                    \frac{1}{2} + \frac{3}{4\sqrt{5}} x - \frac{3}{20\sqrt{5}}x^3
-                    & \text{, if } x\in[-\sqrt{5}:\sqrt{5}] \\
-                    0 & \text{, if } x < -\sqrt{5} \\
-                    1 & \text{, if } x > \sqrt{5}
-                    \end{array}\right.
         """
-        return kernels_imp.epanechnikov_cdf(xs, out)
-
-    def pm1(self, xs, out=None):
-        r"""
-        First partial moment of the distribution:
-
-        .. math::
-
-            \text{pm1}(x) = \left\{\begin{array}{ll}
-                    -\frac{3\sqrt{5}}{16}\left(1-\frac{2}{5}x^2+\frac{1}{25}x^4\right)
-                    & \text{, if } x\in[-\sqrt{5}:\sqrt{5}] \\
-                    0 & \text{, otherwise}
-                    \end{array}\right.
+        Return the CDF of the normal kernel
         """
-        return kernels_imp.epanechnikov_pm1(xs, out)
+        xs = np.asarray(xs)
+        if out is None:
+            out = np.empty(xs.shape[1:], dtype=float)
+        tmp = erf(xs / np.sqrt(2))
+        tmp += 1
+        np.prod(tmp, axis=0, out=out)
+        out /= 2**self.ndim
+        return out
 
-    def pm2(self, xs, out=None):
-        r"""
-        Second partial moment of the distribution:
-
-        .. math::
-
-            \text{pm2}(x) = \left\{\begin{array}{ll}
-                    \frac{5}{20}\left(2 + \frac{1}{\sqrt{5}}x^3 - \frac{3}{5^{5/2}}x^5 \right)
-                    & \text{, if } x\in[-\sqrt{5}:\sqrt{5}] \\
-                    0 & \text{, if } x < -\sqrt{5} \\
-                    1 & \text{, if } x > \sqrt{5}
-                    \end{array}\right.
-        """
-        return kernels_imp.epanechnikov_pm2(xs, out)
-
-    def fft(self, z, out=None):
-        r"""
-        FFT of the Epanechnikov kernel:
-
-        .. math::
-
-            \text{FFT}(w) = \frac{3}{w'^3}\left( \sin w' - w' \cos w' \right)
-
-        where :math:`w' = w\sqrt{5}`
-        """
-        return kernels_imp.epanechnikov_fft(z, out)
-
-    def fft_xfx(self, z, out=None):
-        r"""
-        .. math::
-
-            \text{FFT}(w E(w)) = \frac{3\sqrt{5}i}{w'^4}\left( 3 w' \cos w' - 3 \sin w' + w'^2 \sin w' \right)
-
-        where :math:`w' = w\sqrt{5}`
-        """
-        return kernels_imp.epanechnikov_fft_xfx(z, out)
-
-
-    def dct(self, z, out=None):
-        return kernels_imp.epanechnikov_fft(z, out)
-
-class Epanechnikov_order4(Kernel1D):
-    r"""
-    Order 4 Epanechnikov kernel. That is:
-
-    .. math::
-
-        K_{[4]}(x) = \frac{3}{2} K(x) + \frac{1}{2} x K'(x) = -\frac{15}{8}x^2+\frac{9}{8}
-
-    where :math:`K` is the non-normalized Epanechnikov kernel.
-    """
-
-    upper = 1
-    lower = -upper
-    cut = upper
-
-    def pdf(self, xs, out=None):
-        return kernels_imp.epanechnikov_o4_pdf(xs, out)
     __call__ = pdf
 
-    def cdf(self, xs, out=None):
-        return kernels_imp.epanechnikov_o4_cdf(xs, out)
 
-    def pm1(self, xs, out=None):
-        return kernels_imp.epanechnikov_o4_pm1(xs, out)
-
-    def pm2(self, xs, out=None):
-        return kernels_imp.epanechnikov_o4_pm2(xs, out)
-
-
-class normal_order4(Kernel1D):
-    r"""
-    Order 4 Normal kernel. That is:
-
-    .. math::
-
-        \phi_{[4]}(x) = \frac{3}{2} \phi(x) + \frac{1}{2} x \phi'(x) = \frac{1}{2}(3-x^2)\phi(x)
-
-    where :math:`\phi` is the normal kernel.
-
-    """
-
-    lower = -np.inf
-    upper = np.inf
-    cut = 3.
-
-    def pdf(self, xs, out=None):
-        return kernels_imp.normal_o4_pdf(xs, out)
-    __call__ = pdf
-
-    def cdf(self, xs, out=None):
-        return kernels_imp.normal_o4_cdf(xs, out)
-
-    def pm1(self, xs, out=None):
-        return kernels_imp.normal_o4_pm1(xs, out)
-
-    def pm2(self, xs, out=None):
-        return kernels_imp.normal_o4_pm2(xs, out)
+from .kernels1d import *
+from .kernelsnd import *
 
 kernels1D = [normal_kernel1d, tricube, Epanechnikov, Epanechnikov_order4, normal_order4]
 kernelsnD = [normal_kernel]
